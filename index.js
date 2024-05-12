61f22bd75251608bda7bdb69bd8b2ff37e5730b0
ScheckJ <scheckjah@yahoo.com>
2024-05-12 15:13:38 -0400

    required inquirer package and updated questions array. also created write to file function

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    'What is the title of your project?',
+    'Describe your project',
+    'What are the installation instructions?',
+    'How should your project be used?',
+    'Contributions?',
+    'Test instructions?'
+];
+console.log(questions)
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data), (err) =>
+        err ? console.log(err) : console.log('Success!')
+)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
